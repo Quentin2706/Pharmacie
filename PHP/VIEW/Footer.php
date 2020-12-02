595d4f1484db2805ac4bd3e8a81c2f40255e6425
Quentin2706 <quentin.balair2706@gmail.com>
2020-12-02 14:32:32 +0100

    ajout css boutons Quentin

---
+++ PHP/VIEW/Footer.php
@@ -0,0 +1,20 @@
+<footer>
+        <div class="flex">
+            <div class="centre">Contact</div>
+        </div>
+        <div class="colonne flex">
+            <div class="centre">Adresse Postal</div>
+            <div class="centre">Adresse Mail</div>
+            <div class="centre">N° Telephone</div>
+            <div class="centre">N° SIRET</div>
+        </div>
+        <div class="colonne flex">
+            <div class="centre">Reseaux</div>
+        </div>
+        <div class="logo">
+            <img src="./IMG/logoPharmacie.jpg" alt="logo Pharmacie">
+        </div>
+    </footer>
+</body>
+
+</html>
--- PHP/VIEW/Header.php
+++ PHP/VIEW/Header.php
@@ -1,7 +1,7 @@
 <body class="colonne">
     <header>
         <div class="logo">
-            <img src="../IMG/logoPharmacie.jpg" alt="logo Pharmacie">
+            <img src="./IMG/logoPharmacie.jpg" alt="logo Pharmacie">
         </div>
         <div class="titre">Bienvenue sur le site de la Pharmacie</div>
         <div class="espace"></div>
