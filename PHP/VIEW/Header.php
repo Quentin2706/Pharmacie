7f168158002d6ec3c2daa5ea4032813bf04187a5
Quentin2706 <quentin.balair2706@gmail.com>
2020-12-02 14:20:36 +0100

    decoupe des view en php

---
+++ PHP/VIEW/Header.php
@@ -0,0 +1,8 @@
+<body class="colonne">
+    <header>
+        <div class="logo">
+            <img src="../IMG/logoPharmacie.jpg" alt="logo Pharmacie">
+        </div>
+        <div class="titre">Bienvenue sur le site de la Pharmacie</div>
+        <div class="espace"></div>
+    </header>
