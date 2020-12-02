3ddd9f2d76d19a8862dec2883b3e4cfba0a81bd2
S-Kirua26 <steevengheeraert26pgmail.com>
2020-12-02 14:57:35 +0100

    02/12/2020 SG

---
+++ PHP/VIEW/ListeProduits.php
@@ -0,0 +1,30 @@
+<?php
+
+$produits = ProduitsManager::getList();
+
+echo '<div class="contenu colonne">
+    <div class="margin">
+        <div class="espace"></div>
+        <div class="ajouter"><a class="centre size" href="index.php?page=formulaireProduits&typeFormulaire=ajouter>Ajouter un Produit</a></div>
+        <div class="espace"></div>
+    </div>';
+    foreach ($produits as $unProduits)
+    {
+        echo '<div class="liste">
+            <div class="libelle centre marginBouton size">Libelle Produit</div>
+            <div class="details centre marginBouton"><a class="centre size" href="index.php?page=formulaireProduits&typeFormulaire=details&id='.$unProduits->getIdProduit().'">Details</a></div>
+            <div class="modifier centre marginBouton"><a class="centre size" href="index.php?page=formulaireProduits&typeFormulaire=modifier&id='.$unProduits->getIdProduit().'">Modifier</a></div>
+            <div class="supprimer centre marginBouton"><a class="centre size" href="index.php?page=formulaireProduits&typeFormulaire=supprimer&id='.$unProduits->getIdProduit().'">Supprimer</a></div>
+        </div>
+        <div class="margin">
+            <div class="espace"></div>
+            <div class="return"><a class="centre size" href="index.php?page=ListeProduits">Retour</a></div>
+            <div class="espace"></div>
+        </div>';
+    }
+
+echo '</div>
+
+</body>
+
+</html>';
